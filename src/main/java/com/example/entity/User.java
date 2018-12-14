800f2c0b525d0bf4c2831fd1de477cc0c4dccb49
NnnLillian <weijingwen35@foxmail.com>
2018-12-14 20:18:26 +0800

    Add lombok plugin

--- src/main/java/com/example/entity/User.java
+++ src/main/java/com/example/entity/User.java
@@ -1,43 +1,14 @@
 package com.example.entity;
 
+import lombok.Data;
 
+@Data
 public class User {
     private String userName;
     private String userId;
     private String userJob;
     private int userLevel;
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public String getUserJob() {
-        return userJob;
-    }
-
-    public void setUserJob(String userJob) {
-        this.userJob = userJob;
-    }
-
-    public int getUserLevel() {
-        return userLevel;
-    }
-
-    public void setUserLevel(int userLevel) {
-        this.userLevel = userLevel;
-    }
 
     public User() {
 
